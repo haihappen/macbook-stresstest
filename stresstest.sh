7fc63b4c583491402a9683d672e1db3b04101c6d
Mario Uher <uher.mario@gmail.com>
2015-05-23 16:49:16 +0200

    Improves stresstest.

--- stresstest.sh
+++ stresstest.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-n=0; time while (( n++ < 5 )); do docker-compose build ; done
+n=0; time while (( n++ < 5 ));
+do
+  docker build .
+  # We need to touch something, otherwise docker hits the cache.
+  touch Gemfile
+done
